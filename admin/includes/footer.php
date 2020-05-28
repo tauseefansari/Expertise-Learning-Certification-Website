e0548f9599a889e9ce6bd51f83354180a5b7e3da
tauseefansari <53376510+tauseefansari@users.noreply.github.com>
2020-05-28 20:21:04 +0530

    Completed with Certificate Option

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="footer-area section_gap">
+        <div class="container" align="center">
+
+    <h2 align="center" style="color:#fff"> Admin Panel of Expertise Learning</h2>
+        </div>
+    </footer>
---
+++ admin/includes/header.php
@@ -0,0 +1,29 @@
+<!--================ Start Header Menu Area =================-->
+    <header class="header_area">
+        
+        <div class="main_menu">
+            
+
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="container">
+                   <h3><a href="index.php">Admin Console</a></h3>
+                    
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+                        <ul class="nav navbar-nav menu_nav ml-auto">
+                            <li class="nav-item active"><a class="nav-link" href="index.php">Home</a></li>
+                            <li class="nav-item"><a class="nav-link" href="admin/index.php">Login</a></li>
+                          
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <!--================ End Header Menu Area =================-->
